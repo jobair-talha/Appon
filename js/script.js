ad989e83177b617aeff5a91d6d7cf0ffbf4a77db
jobair-talha <78406840+jobair-talha@users.noreply.github.com>
2021-02-02 15:38:04 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$('.banner-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+});
+
+// Screen part start
+$('.screen-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+});
+
+// venobox start
+$('.venobox').venobox();
+
+// Feedback text Slider
+$('.user-text').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '.user-img',
+});
+
+// Feedback img Slider
+$('.user-img').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  asNavFor: '.user-text',
+  centerMode: true,
+  centerPadding: "0",
+  focusOnSelect: true
+});
